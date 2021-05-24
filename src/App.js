db923e9974ef4a9649aa4498ed5bec4b40e78b38
Alan Biju <67945306+Alan-Biju@users.noreply.github.com>
2021-05-24 13:33:45 +0530

    switch change

--- src/App.js
+++ src/App.js
@@ -2,22 +2,27 @@ import React from 'react';
 import styled from 'styled-components';
 import Home from './components/Home/Home';
 import NavBar from './components/NavBar/NavBar';
-import { Route } from 'react-router-dom';
+import { Route,Switch } from 'react-router-dom';
 import MovieDetails from './components/MovieDetails/MovieDetails';
 
 function App() {
 	return (
 		<AppContainer>
 			<NavBar />
-			<Route path='/' default exact>
-				<Home />
-			</Route>
-			<Route path='/tv-shows'  exact>
-				<Home />
-			</Route>
-			<Route path='/info/:category/:id' exact>
-				<MovieDetails />
-			</Route>
+			<Switch>
+				<Route path='/' default exact>
+					<Home />
+				</Route>
+				<Route path='/tv-shows' exact>
+					<Home />
+				</Route>
+				<Route path='/info/:category/:id' exact>
+					<MovieDetails />
+				</Route>
+				<Route path="*">
+					<Home />
+				</Route>
+			</Switch>
 		</AppContainer>
 	);
 }
