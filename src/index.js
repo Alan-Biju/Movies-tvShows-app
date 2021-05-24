45f4ae3ebfa1c04b26aa015343eb4047c4ff9fc1
Alan Biju <67945306+Alan-Biju@users.noreply.github.com>
2021-05-24 13:34:17 +0530

    switch del

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import MovieProvider from './MovieDataContext';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { BrowserRouter as Router} from 'react-router-dom';
 
 ReactDOM.render(
 			<MovieProvider>
 				<Router>
-					<Switch>
+					
 							<App />
-					</Switch>
+					
 				</Router>
 			</MovieProvider>,
 	document.getElementById('root'),
